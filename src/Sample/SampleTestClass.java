aeb3bd153891cb3b4231d4e788d55a441b908dd6
jing039 <tjinghua39@gmail.com>
2024-01-06 20:46:34 -0500

    Second commit.

--- src/Sample/SampleTestClass.java
+++ src/Sample/SampleTestClass.java
@@ -5,6 +5,7 @@ public class SampleTestClass {
 	public static void main(String[] args) {
 
 		System.out.println("This project is commited and pushed to GitHub.");
+		System.out.println("This is the second commit.");
 	}
 
 }
